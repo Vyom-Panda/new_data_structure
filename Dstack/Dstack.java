2fb034ff85e024d75628c276aa8c798df3b8cd69
VYOM <vyompanda99@gmail.com>
2024-06-25 03:15:11 +0530

    initial commit for DSTACK DSA

---
+++ Dstack/Dstack.java
@@ -0,0 +1,12 @@
+package Dstack;
+
+public interface Dstack {
+
+    public void push(int i);
+    public void pop();
+    public int peek();
+    public int size();
+    public void mid();
+    public void show();
+
+}
---
+++ Main.java
@@ -0,0 +1,31 @@
+import Dstack.Dstack;
+import Dstack.DstackImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dstack dst=new DstackImpl();
+
+        System.out.print("Initial stack: ");
+        dst.show();
+        dst.push(10);
+        dst.push(20);
+        dst.push(30);
+        dst.push(40);
+        dst.push(40);
+        dst.push(40);
+        dst.push(40);
+        dst.show();
+        dst.mid();
+        System.out.println("PEEK: "+dst.peek());
+        dst.pop();
+        dst.pop();
+        dst.pop();
+        dst.pop();
+        dst.pop();
+        System.out.print("Final stack: ");
+        dst.show();
+
+    }
+}
